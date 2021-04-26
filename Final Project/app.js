b764459205c952fe1c0783ec1a548f99776a4d10
heishiwu <32119365+heishiwu@users.noreply.github.com>
2021-04-26 17:11:54 -0400

    start
    start

---
+++ Final Project/app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const configRoutes = require('./routes');
+const exphbs = require('express-handlebars');
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+app.use(
+    session({
+        name: 'AuthCookie',
+        secret: 'some secret string!',
+        resave: false,
+        saveUninitialized: true,
+    })
+);
+configRoutes(app);
+app.listen(3000, () =>{
+    console.log("Your routes will be running on http://localhost:3000");});
