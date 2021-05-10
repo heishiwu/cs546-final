28493b76c019fdcc6e017ee0409673347a13c77d
Yi Lin <546654542@qq.com>
2021-05-10 13:58:05 -0400

    This is Yi lin 5.10 1:57PM

--- Final Project/data/users.js
+++ Final Project/data/users.js
@@ -3,6 +3,7 @@ const users = mongoCollections.users;
 ObjectId = require('mongodb').ObjectID;
 const bcrypt = require('bcrypt');
 const saltRounds = 16;
+const moment = require("moment");
 
 
 //create user by some information, only name, username, password, email birthday and insurance are necessary, and username and email are unique.
@@ -29,7 +30,7 @@ const saltRounds = 16;
 //     "race":"Asian",
 //     "ethnicity": "Not Hispanic or latino",
 //     "insurance": {
-//     "insuranceType":"Private Insurance",
+//         "insuranceType":"Private Insurance",
 //         "insuranceName":"HuHu"
 // },
 //     "medicalGroupNumber": "HDEPO National HRA",
@@ -61,7 +62,7 @@ const saltRounds = 16;
  * @param comments_history: Array with a few of commentsId
  * @returns {Promise<void>}
  */
-async function createUser(name, username, password, email, address, birthday, gender, race, ethnicity, insurance, medicalGroupNumber, medicalid, reservation_history, comments_history){
+async function createUser(name, username, password, email, address, birthday, gender, race, ethnicity, insurance, medicalGroupNumber, medicalid){
     if (!name || typeof name !== "object"|| !name.firstName || !name.lastName|| typeof (name.firstName) !=="string" || typeof(name.lastName) !== "string"){
         throw "must provide username";
     }
@@ -106,16 +107,102 @@ async function createUser(name, username, password, email, address, birthday, ge
         throw "must provide birthday";
     }
     //test birthday using regular expression.
+    if(!moment(birthday, "MM/DD/YYYY", true).isValid() &&
+        !moment(birthday, "M/DD/YYYY", true).isValid() &&
+        !moment(birthday, "MM/D/YYYY", true).isValid() &&
+        !moment(birthday, "M/D/YYYY", true).isValid()){
+        throw "must provide correct format birthday";
+    }
 
+    if(gender){
+        if(typeof (gender) !== "string"){
+            throw "must provide string format gender";
+        }
+    }
+
+    if(race){
+        if(typeof (race) !== "string"){
+            throw "must provide string format race";
+        }
+    }
+
+    if(ethnicity){
+        if(typeof (ethnicity) !== "string"){
+            throw "must provide string format ethnicity";
+        }
+    }
+
+    if(!insurance || typeof (insurance) !=="object"){
+        throw "must provide string format insurance";
+    }else {
+        if(!insurance.insuranceType || typeof (insurance.insuranceType) !== "string"){
+            throw "must provide string format insurance.insuranceType";
+        }
+        if(!insurance.insuranceName || typeof (insurance.insuranceName) !== "string"){
+            throw "must provide string format insurance.insuranceName";
+        }
+    }
+
+    if(medicalGroupNumber){
+        if(typeof (medicalGroupNumber) !== "string"){
+            throw "must provide string format medicalGroupNumber";
+        }
+    }
+
+    if(medicalid){
+        if(typeof (medicalid) !== "string"){
+            throw "must provide string format medicalid";
+        }
+    }
+
+    //before create the users, checking the users is unique in email and username
+    const allUsers = await this.getAllUsers();
+    email = email.toLowerCase();
+    username = username.toLowerCase();
+    allUsers.forEach(user => {
+        if (user.email == email) throw 'This email is already taken.';
+        if (user.username == username) throw 'This username is already taken.';
+    })
+
+
+    //create hashed password
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+    let newUser = {
+        name: name,
+        username: username,
+        password: hashedPassword,
+        email: email,
+        address: address,
+        birthday: birthday,
+        gender: gender,
+        race: race,
+        ethnicity: ethnicity,
+        insurance: insurance,
+        medicalGroupNumber: medicalGroupNumber,
+        medicalid: medicalid,
+    };
+
+    const userCollection = await users();
+    const insertInfo = await userCollection.insertOne(newUser);
+
+    if (insertInfo.insertedCount === 0) throw 'Could not add user.';
+
+    const newId = insertInfo.insertedId;
+    const finuser = await this.getUserById(newId.toString());
+
+    return finuser;
 
 };
 
 
 // get user all information by userId
 async function getUserById(userId){
-    if (!verify.validString(userId)){
+    if (!userId || typeof userId !== 'string' || !userId.trim()){
         throw 'User id is not a valid string.';
     }
+    const objId = ObjectId.createFromHexString(userId);
+    userId = objId;
     const userCollection = await users();
     let user = await userCollection.findOne({_id: userId});
     if(user === null){
@@ -132,23 +219,179 @@ async function getAllUsers(){
     return allUsers;
 };
 
-async function updateUsername(){
+async function updateUsername(userId, username){
+    if (!userId || typeof userId !== 'string' || !userId.trim()){
+        throw 'User id is not a valid string.';
+    }
+    if (!username || typeof username !== "string"){
+        throw 'you should input a string as the username';
+    }
+    let userObjId = ObjectId.createFromHexString(userId);
+    const userCollection = await users();
+    let userInformation = await userCollection.findOne({username: username});
+    if(userInformation !== null){
+        throw "the username is already exisited";
+    }else {
+        let userUpdateInfo = {
+            username: username
+        };
+        let updatedInfo = await userCollection.updateOne({ _id: userObjId }, { $set: userUpdateInfo });
+        if (updatedInfo.modifiedCount === 0) {
+            throw 'could not edit the username successfully';
+        }
+        return this.getUserById(userId);
+
+    }
+
 
 };
 
-async function updatePassword(){
+async function updatePassword(userId, newPassword){
+    if (!userId || typeof userId !== 'string' || !userId.trim()){
+        throw 'User id is not a valid string.';
+    }
+    if (!newPassword || typeof newPassword !== "string"){
+        throw 'you should input a string as the newPassword';
+    }
+
+    let userObjId = ObjectId.createFromHexString(userId);
+    const userCollection = await users();
+
+    const hashedPassword = await bcrypt.hash(newPassword, saltRounds);
+    let userUpdateInfo = {
+        password: hashedPassword
+    };
+    let updatedInfo = await userCollection.updateOne({ _id: userObjId }, { $set: userUpdateInfo });
+    if (updatedInfo.modifiedCount === 0) {
+        throw 'could not edit the username successfully';
+    }
+    return this.getUserById(userId);
+
 
 };
 
 
 
-async function updateUserInformation(){
+async function updateUserInformation(userId, name, email, address, birthday, gender, race, ethnicity, insurance, medicalGroupNumber, medicalid){
+    if (!userId || typeof userId !== 'string' || !userId.trim()){
+        throw 'User id is not a valid string.';
+    }
+    if(name){
+        if ( typeof name !== "string"){
+            throw "name should be string format";
+        }
+    }
+
+    if(email){
+        if((/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/).test(email)){
+            throw "must provide correct format email";
+        }
+    }
+
+    if(address){
+        if(!address.addressLine || typeof (address.addressLine) !=="string"){
+            throw "must provide addressLine";
+        }
+        if(!address.apartment_suite_unitNumber || typeof (address.apartment_suite_unitNumber) !=="string"){
+            throw "must provide apartment_suite_unitNumber";
+        }
+        if(!address.city || typeof (address.city) !=="string"){
+            throw "must provide city";
+        }
+        if(!address.county || typeof (address.county) !=="string"){
+            throw "must provide county";
+        }
+        if(!address.state || typeof (address.state) !=="string"){
+            throw "must provide county";
+        }
+        if(!address.postalCode || typeof (address.postalCode) !=="string"){
+            throw "must provide postalCode";
+        }
+        //test postalCode using regular expression.
+        if((/^[0-9]{5}(?:-[0-9]{4})?$/).test(address.postalCode)){
+            throw "must provide correct format postalCode";
+        }
+    }
+
+    if(!moment(birthday, "MM/DD/YYYY", true).isValid() &&
+        !moment(birthday, "M/DD/YYYY", true).isValid() &&
+        !moment(birthday, "MM/D/YYYY", true).isValid() &&
+        !moment(birthday, "M/D/YYYY", true).isValid()){
+        throw "must provide correct format birthday";
+    }
+
+    if(gender){
+        if(typeof (gender) !== "string"){
+            throw "must provide string format gender";
+        }
+    }
+
+    if(race){
+        if(typeof (race) !== "string"){
+            throw "must provide string format race";
+        }
+    }
+
+    if(ethnicity){
+        if(typeof (ethnicity) !== "string"){
+            throw "must provide string format ethnicity";
+        }
+    }
+
+    if(!insurance || typeof (insurance) !=="object"){
+        throw "must provide string format insurance";
+    }else {
+        if(!insurance.insuranceType || typeof (insurance.insuranceType) !== "string"){
+            throw "must provide string format insurance.insuranceType";
+        }
+        if(!insurance.insuranceName || typeof (insurance.insuranceName) !== "string"){
+            throw "must provide string format insurance.insuranceName";
+        }
+    }
+
+    if(medicalGroupNumber){
+        if(typeof (medicalGroupNumber) !== "string"){
+            throw "must provide string format medicalGroupNumber";
+        }
+    }
+
+    if(medicalid){
+        if(typeof (medicalid) !== "string"){
+            throw "must provide string format medicalid";
+        }
+    }
+
+    let userObjId = ObjectId.createFromHexString(userId);
+    const userCollection = await users();
+    let userInformation = await userCollection.findOne({email: email});
+    if(userInformation !== null){
+        throw "the email is already exisited";
+    }else {
+        let userUpdateInfo = {
+            name: name,
+            email: email,
+            address: address,
+            birthday: birthday,
+            gender: gender,
+            race: race,
+            ethnicity: ethnicity,
+            insurance: insurance,
+            medicalGroupNumber: medicalGroupNumber,
+            medicalid: medicalid,
+        };
+        let updatedInfo = await userCollection.updateOne({ _id: userObjId }, { $set: userUpdateInfo });
+        if (updatedInfo.modifiedCount === 0) {
+            throw 'could not edit the username successfully';
+        }
+        return this.getUserById(userId);
+    }
+
+
+
+
 
 };
 
-async function removeUser(){
-
-};
 
 
 
