cdb4c75b17f680258f7708d08c23f4ff9b2afc1a
Lvzoey <lzhzoey@gmail.com>
2021-05-13 16:02:41 -0400

    from Zehui Lu

--- Final Project/app.js
+++ Final Project/app.js
@@ -1,14 +1,23 @@
 const express = require("express");
 const app = express();
+const static = express.static(__dirname + '/public');
 const session = require('express-session');
 const cookieParser = require('cookie-parser');
 const configRoutes = require('./routes');
 const exphbs = require('express-handlebars');
+
+app.use('/public', static);
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.engine('handlebars', exphbs({
+    extname: 'handlebars',
+    defaultLayout: 'main',
+    layoutsDir: __dirname + '/views/layouts/',
+    partialsDir: __dirname + '/views/partials/'
+}));
 app.set("view engine", "handlebars");
+
 app.use(
     session({
         name: 'AuthCookie',
