73ab73cd23f4633bc5447f60d1d3193f7f4d7ca6
heishiwu <32119365+heishiwu@users.noreply.github.com>
2021-05-14 15:32:11 -0400

    1
    1

--- Final Project/routes/private.js
+++ Final Project/routes/private.js
@@ -2,21 +2,43 @@ const express = require("express");
 const router = express.Router();
 const data = require("../data");
 const userData = data.users;
+const adminData = data.administration;
+// router.get('/', async (req, res) =>{
+//     try{
+//         let userInformation;
+//         if(req.session){
+//             if(req.session.userId){
+//                 userInformation = await userData.getUserById(req.params.userId);
+//             } 
+//         }
+//             res.render('landing/landing', {userInformation,partial:'landing-script'});
+       
+//     }catch (e){
+//         res.redirect('/private');
+//     }
+// });
+
 
 router.get('/', async (req, res) =>{
     try{
         let userInformation;
+        let adminInformation;
         if(req.session){
             if(req.session.userId){
                 userInformation = await userData.getUserById(req.params.userId);
+            } else if(req.session.adminId){
+                adminInformation = await adminData.getAdminById(req.params.userId);
             }
         }
-        res.render('landing/landing', {userInformation,partial:'landing-script'});
+        if(req.session.adminId){
+            res.render('admin/admin', {adminInformation,partial:'admin-script'}); 
+        } else {
+            res.render('landing/landing', {userInformation,partial:'landing-script'});
+        }
     }catch (e){
         res.redirect('/private');
     }
 });
-
 
 // router.get('/search', async (req, res) =>{
 //
